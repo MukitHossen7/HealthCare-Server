79d3811333e6edb24cd0e656e2dfb99af76ac0d2
MukitHossen7 <hossenmukit7@gmail.com>
2025-10-07 00:14:50 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+import cors from "cors";
+import express, { Request, Response } from "express";
+
+import config from "./config";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
+import notFound from "./middlewares/notFound";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    success: true,
+    message: "Welcome to Health Care Server",
+    environment: config.NODE_ENV,
+  });
+});
+
+app.use(globalErrorHandler);
+app.use(notFound);
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,50 @@
+import { Server } from "http";
+import app from "./app";
+import config from "./config";
+
+let server: Server;
+
+const healthCareServer = async () => {
+  try {
+    // await prisma.$connect();
+    // console.log("✅ Connected to the database successfully.");
+    server = app.listen(config.PORT, () => {
+      console.log(`🚀 Server is running on http://localhost: ${config.PORT}`);
+    });
+  } catch (error) {
+    console.log("❌ Database connection failed:", error);
+    process.exit(1);
+  }
+};
+
+healthCareServer();
+
+//Server error handle
+process.on("unhandledRejection", (err) => {
+  console.log("unHandle rejection detected... Server shutting down... ", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on("uncaughtException", (err) => {
+  console.log("un caught exception detected... Server shutting down... ", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on("SIGTERM", () => {
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
