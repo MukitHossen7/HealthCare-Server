62ea6cf2c77f5750ba2005b6944fafe4459b6505
MukitHossen7 <hossenmukit7@gmail.com>
2025-10-21 12:23:37 +0600

    Refactor createAppointment function to enhance appointment creation logic and ensure proper handling of doctor schedules and video calling IDs.

--- src/modules/appointment/appointment.service.ts
+++ src/modules/appointment/appointment.service.ts
@@ -1,12 +1,62 @@
 import { Appointment } from "@prisma/client";
 import { IJwtPayload } from "../../types/common";
-
+import { prisma } from "../../utils/prisma";
+import { v4 as uuidv4 } from "uuid";
 const createAppointment = async (
   payload: Partial<Appointment>,
   user: IJwtPayload
 ) => {
-  console.log(payload);
-  console.log(user);
+  const patientData = await prisma.patient.findUniqueOrThrow({
+    where: {
+      email: user.email,
+    },
+  });
+
+  const doctorData = await prisma.doctor.findUniqueOrThrow({
+    where: {
+      id: payload.doctorId,
+      user: {
+        isDeleted: false,
+      },
+    },
+  });
+
+  await prisma.doctorSchedules.findFirstOrThrow({
+    where: {
+      doctorId: doctorData.id,
+      scheduleId: payload.scheduleId,
+      isBooked: false,
+    },
+  });
+
+  const videoCallingId = uuidv4();
+
+  const result = await prisma.$transaction(async (tnx) => {
+    const appointmentData = await tnx.appointment.create({
+      data: {
+        patientId: patientData.id,
+        doctorId: doctorData.id,
+        scheduleId: payload.scheduleId as string,
+        videoCallingId: videoCallingId,
+      },
+    });
+
+    await tnx.doctorSchedules.update({
+      where: {
+        doctorId_scheduleId: {
+          doctorId: doctorData.id,
+          scheduleId: payload.scheduleId as string,
+        },
+      },
+      data: {
+        isBooked: true,
+      },
+    });
+
+    return appointmentData;
+  });
+
+  return result;
 };
 
 export const appointmentServices = {
