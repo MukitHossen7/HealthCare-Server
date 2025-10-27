0268679ad2efe9cf0066236ebd8c76759f1a8955
MukitHossen7 <hossenmukit7@gmail.com>
2025-10-27 16:17:19 +0600

    create get admin meta data functionlity

--- src/modules/meta/meta.controller.ts
+++ src/modules/meta/meta.controller.ts
@@ -2,16 +2,20 @@ import { Request, Response } from "express";
 import catchAsync from "../../utils/catchAsync";
 import sendResponse from "../../utils/sendResponse";
 import { metaServices } from "./meta.service";
+import { IJwtPayload } from "../../types/common";
 
-const getDashboardMetaData = catchAsync(async (req: Request, res: Response) => {
-  const result = await metaServices.getDashboardMetaData();
-  sendResponse(res, {
-    statusCode: 200,
-    success: true,
-    message: "Get dashboard metadata successfully",
-    data: result,
-  });
-});
+const getDashboardMetaData = catchAsync(
+  async (req: Request & { user?: IJwtPayload }, res: Response) => {
+    const user = req.user as IJwtPayload;
+    const result = await metaServices.getDashboardMetaData(user);
+    sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: "Get dashboard metadata successfully",
+      data: result,
+    });
+  }
+);
 
 export const metaController = {
   getDashboardMetaData,
--- src/modules/meta/meta.service.ts
+++ src/modules/meta/meta.service.ts
@@ -1,4 +1,44 @@
-const getDashboardMetaData = async () => {};
+import { PaymentStatus, UserRole } from "@prisma/client";
+import { IJwtPayload } from "../../types/common";
+import AppError from "../../errorHelpers/AppError";
+import httpStatus from "http-status";
+import { prisma } from "../../utils/prisma";
+
+const getDashboardMetaData = async (user: IJwtPayload) => {
+  let metadata;
+
+  switch (user.role) {
+    case UserRole.ADMIN:
+      metadata = "admin meta data";
+      break;
+    case UserRole.DOCTOR:
+      metadata = "doctor meta data";
+      break;
+    case UserRole.PATIENT:
+      metadata = "patient meta data";
+      break;
+    default:
+      throw new AppError(httpStatus.BAD_REQUEST, "Invalid user role");
+  }
+  return metadata;
+};
+
+const getAdminMetaData = async () => {
+  const totalPatient = await prisma.patient.count();
+  const totalDoctor = await prisma.doctor.count();
+  const totalAdmin = await prisma.admin.count();
+  const totalAppointment = await prisma.appointment.count();
+  const totalPayment = await prisma.payment.count();
+
+  const totalRevenue = await prisma.payment.aggregate({
+    _sum: {
+      amount: true,
+    },
+    where: {
+      status: PaymentStatus.PAID,
+    },
+  });
+};
 
 export const metaServices = {
   getDashboardMetaData,
