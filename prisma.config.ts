3d248a1401347d724c2b9ae9265967b31dc1b227
MukitHossen7 <hossenmukit7@gmail.com>
2025-11-20 22:17:26 +0600

    feat: integrate Prisma with PostgreSQL adapter and update dependencies

---
+++ prisma.config.ts
@@ -0,0 +1,9 @@
+// import { defineConfig } from "@prisma/config";
+
+// const config = defineConfig({
+//   datasource: {
+//     url: process.env.DATABASE_URL!,
+//   },
+// });
+
+// export default config;
--- src/config/db.ts
+++ src/config/db.ts
@@ -1,3 +1,23 @@
 import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient();
+// import { PrismaClient } from "@prisma/client";
+
+// const prisma = new PrismaClient({
+// adapter: "postgresql",
+// });
+
+// import { PrismaClient } from "@prisma/client";
+// import { Pool } from "pg";
+// // import { PgAdapter } from "@prisma/adapter-pg";
+// import { PgAdapter } from "@prisma/adapter-pg";
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+// });
+
+// const adapter = new PgAdapter(pool);
+
+// const prisma = new PrismaClient({ adapter });
+
+// export default prisma;
